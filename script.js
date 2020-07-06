3d27ad040e810ad566645944ff4bfab70385e77e
283-سيد حامد المهري <66621181+halmohri22@users.noreply.github.com>
2020-07-06 22:38:09 +0300

    only finished html and css haven't finished js

---
+++ script.js
@@ -0,0 +1,11 @@
+// let potato =[
+//     "french fries",
+//     "potato soup",
+//     "tater tots",
+// ]
+
+// function product1(){
+//     document.getElementById("product1_r").innerHTML = "french fries";
+// }
+
+
